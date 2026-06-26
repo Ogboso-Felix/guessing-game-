ab9df72ef2ef24b98b0120daa146d5ec34898494
Ogboso Felix <ogbosoogboso35@gmail.com>
2026-06-26 12:42:30 +0100

    Add difficulty level selection

--- Complete_basic_guessing_game.py
+++ Complete_basic_guessing_game.py
@@ -1,10 +1,27 @@
 import random
-secret_number = random.randint(1,600)
+print("=" * 40)
+print("WELCOME TO MY NUMBER GUESSING GAME")
+print("=" * 40)
+print("\nChoose a Difficulty")
+print("1. EASY")
+print("2. MEDIUM")
+print("3. HARD")
+choice = int(input("Enter your choice:"))
+if choice == 1:
+    maximum = 50
+elif choice == 2:
+    maximum = 200
+elif choice == 3:
+    maximum = 600
+else:
+    print("Invalid choice. Defaulting to Hard difficulty.")
+    maximum = 600
+secret_number = random.randint(1, maximum)
 attempts = 0
-print("Hello welcome to my number guessing game. \n I am thinking of a number between 1 and 600")
+print(f"Great! I am thinking of a number between 1 and {maximum}")
 while True:
     guess = int(input("What is the number?"))
-    if guess < 1 or guess > 600:
+    if guess < 1 or guess > maximum:
         print("Follow the rule of the game!")
         continue
     attempts +=1
