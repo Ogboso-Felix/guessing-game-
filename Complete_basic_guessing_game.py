1defe9156d8d3721e44989f8a8e8cc7e55fd9171
Ogboso Felix <ogbosoogboso35@gmail.com>
2026-07-16 12:07:02 +0100

    score-system

--- Complete_basic_guessing_game.py
+++ Complete_basic_guessing_game.py
@@ -1,4 +1,6 @@
 import random
+yes_answers = ["yes", "y"]
+no_answers = ["no", "n"]
 while True:
     print("=" * 40)
     print("WELCOME TO MY NUMBER GUESSING GAME")
@@ -12,12 +14,15 @@ while True:
             choice = int(input("Enter your choice:"))
             if choice == 1:
                 maximum = 50
+                attempts_limit = 11
                 break
             elif choice == 2:
                 maximum = 200
+                attempts_limit = 8
                 break
             elif choice == 3:
                 maximum = 600
+                attempts_limit = 6
                 break
             else:
                 print("Invalid choice. Choose 1, 2, or 3.")
@@ -31,15 +36,41 @@ while True:
             guess = int(input("What is the number?"))
             if guess < 1 or guess > maximum:
                 print("Follow the rule of the game!")
-                #continue
+                continue
             attempts +=1
-            if guess > secret_number:
-                print("too high, try again!")
+            if guess ==secret_number:
+                print("Congratulations!")
+                if attempts == 1:
+                    print (f"You guessed the number in {attempts} attempt")
+                else:
+                    print(f"You guessed the number in {attempts} attempts")
+                    break
+            if attempts >= attempts_limit:
+                print ("GAME OVER")
+                print(f"The number was {secret_number}")
+                print("Better luck next time")
+                break
+            elif guess > secret_number:
+                print("Too high, try again!")
+                print(f"You have {attempts_limit - attempts} attempts remaining")
             elif guess < secret_number:
-                print("too low, try again!")
+                print("Too low, try again!")
+                print(f"You have {attempts_limit - attempts} attempts remaining")
             else:
-                print(f"correct! You guessed the number in {attempts} attempts")
+                print(f"You guessed the number in {attempts} attempts")
                 break
         except ValueError:
             print("Please enter a valid number")
-            
+            #Ask ↓  Validate  ↓ Invalid? ↓ Tell the user ↓ Ask again
+    while True:
+            print("Would you like to play the game again \n Y/N")
+            response = input("Enter your choice: ")
+            response = response.lower()
+            if response in yes_answers:
+                break
+            elif response in no_answers:
+                print("Goodbye!")
+                exit()
+            else:
+                print("Invalid choice. Please enter Y or N")
+                continue
--- attempt_limit.py
+++ attempt_limit.py
@@ -2,6 +2,7 @@
 import random
 yes_answers = ["yes", "y"]
 no_answers = ["no", "n"]
+score = 0
 while True:
     print("=" * 40)
     print("WELCOME TO MY NUMBER GUESSING GAME")
@@ -16,14 +17,20 @@ while True:
             if choice == 1:
                 maximum = 50
                 attempts_limit = 11
+                base_score = 100
+                difficulty = "EASY"
                 break
             elif choice == 2:
                 maximum = 200
                 attempts_limit = 8
+                base_score = 200
+                difficulty = "MEDIUM"
                 break
             elif choice == 3:
                 maximum = 600
                 attempts_limit = 6
+                base_score = 300
+                difficulty = "HARD"
                 break
             else:
                 print("Invalid choice. Choose 1, 2, or 3.")
@@ -40,12 +47,25 @@ while True:
                 continue
             attempts +=1
             if guess ==secret_number:
+                remaining_attempts = attempts_limit - attempts
+                bonus = remaining_attempts * 10
+                points_earned = base_score + bonus
+                score += points_earned
+                print ("=" * 40)
                 print("Congratulations!")
+                print ("=" * 40)
+                print()
+                print(f"Difficulty:     {difficulty}")
+                print(f"Basescore:      {base_score}")
+                print(f"Bonus:          {bonus} ")
+                print("-" * 40)
+                print(f"Points Earned:  {points_earned}")
+                print(f"Current Score:  {score}")
                 if attempts == 1:
                     print (f"You guessed the number in {attempts} attempt")
                 else:
                     print(f"You guessed the number in {attempts} attempts")
-                    break
+                break
             if attempts >= attempts_limit:
                 print ("GAME OVER")
                 print(f"The number was {secret_number}")
@@ -70,7 +90,13 @@ while True:
             if response in yes_answers:
                 break
             elif response in no_answers:
+                print("=" * 40)
+                print(f"Final score:    {score}")
+                print()
+                print("Thank you for playing!")
+                print()
                 print("Goodbye!")
+                print("=" * 40)
                 exit()
             else:
                 print("Invalid choice. Please enter Y or N")
